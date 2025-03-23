ff602e82900b275d573f314c0f93913f3742f70a
Dimas <dimasriat@gmail.com>
2025-03-24 03:01:49 +0700

    feat: add brewhouse and test

---
+++ src/BrewHouse.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IWETH9 as IWETH} from "./interfaces/IWETH9.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract BrewHouse is ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    error InsufficientCollateral();
+    error OnlySequencer();
+
+    address public sequencer;
+    IWETH public weth;
+
+    mapping(address => uint256) public collateralBalance;
+
+    constructor(address _sequencer, address _weth) {
+        sequencer = _sequencer;
+        weth = IWETH(_weth);
+    }
+
+    function depositCollateral() external payable nonReentrant {
+        weth.deposit{value: msg.value}();
+        collateralBalance[msg.sender] += msg.value;
+    }
+
+    function withdrawCollateral(uint256 amount) external nonReentrant {
+        if (collateralBalance[msg.sender] < amount) revert InsufficientCollateral();
+        collateralBalance[msg.sender] -= amount;
+        weth.withdraw(amount);
+        payable(msg.sender).transfer(amount);
+    }
+
+    function slashCollateral(address user, uint256 amount) external onlySequencer {
+        collateralBalance[user] -= amount;
+        weth.withdraw(amount);
+        payable(sequencer).transfer(amount);
+    }
+
+    receive() external payable {}
+
+    modifier onlySequencer() {
+        if (msg.sender != sequencer) revert OnlySequencer();
+        _;
+    }
+}
---
+++ test/BrewHouse.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/BrewHouse.sol";
+import "../src/interfaces/IWETH9.sol";
+import "./MockWETH.sol";
+
+contract BrewHouseTest is Test {
+    BrewHouse public brewHouse;
+    MockWETH public weth;
+
+    address sequencer = vm.addr(1);
+    address user = vm.addr(2);
+
+    function setUp() public {
+        weth = new MockWETH();
+        vm.deal(user, 10 ether);
+        vm.deal(sequencer, 10 ether);
+        brewHouse = new BrewHouse(sequencer, address(weth));
+    }
+
+    function testDepositCollateral() public {
+        vm.startPrank(user);
+        brewHouse.depositCollateral{value: 1 ether}();
+        assertEq(brewHouse.collateralBalance(user), 1 ether);
+        vm.stopPrank();
+    }
+
+    function testWithdrawCollateral() public {
+        vm.startPrank(user);
+        brewHouse.depositCollateral{value: 2 ether}();
+        brewHouse.withdrawCollateral(1 ether);
+        assertEq(brewHouse.collateralBalance(user), 1 ether);
+        vm.stopPrank();
+    }
+
+    function testWithdrawMoreThanCollateralShouldRevert() public {
+        vm.startPrank(user);
+        brewHouse.depositCollateral{value: 1 ether}();
+        vm.expectRevert(BrewHouse.InsufficientCollateral.selector);
+        brewHouse.withdrawCollateral(2 ether);
+        vm.stopPrank();
+    }
+
+    function testOnlySequencerCanSlash() public {
+        vm.startPrank(user);
+        brewHouse.depositCollateral{value: 1 ether}();
+        vm.stopPrank();
+
+        vm.startPrank(address(3)); // Not sequencer
+        vm.expectRevert(BrewHouse.OnlySequencer.selector);
+        brewHouse.slashCollateral(user, 0.5 ether);
+        vm.stopPrank();
+    }
+
+    function testSequencerCanSlash() public {
+        vm.startPrank(user);
+        brewHouse.depositCollateral{value: 1 ether}();
+        vm.stopPrank();
+
+        vm.startPrank(sequencer);
+        brewHouse.slashCollateral(user, 0.5 ether);
+        assertEq(brewHouse.collateralBalance(user), 0.5 ether);
+        vm.stopPrank();
+    }
+}
+
