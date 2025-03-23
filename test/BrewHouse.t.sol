6a838664f287f8877b54c5da29848b3b82a0f0aa
Dimas <dimasriat@gmail.com>
2025-03-24 04:14:38 +0700

    chore: add prettier

--- test/BrewHouse.t.sol
+++ test/BrewHouse.t.sol
@@ -7,62 +7,61 @@ import "../src/interfaces/IWETH9.sol";
 import "./MockWETH.sol";
 
 contract BrewHouseTest is Test {
-    BrewHouse public brewHouse;
-    MockWETH public weth;
+  BrewHouse public brewHouse;
+  MockWETH public weth;
 
-    address sequencer = vm.addr(1);
-    address user = vm.addr(2);
+  address sequencer = vm.addr(1);
+  address user = vm.addr(2);
 
-    function setUp() public {
-        weth = new MockWETH();
-        vm.deal(user, 10 ether);
-        vm.deal(sequencer, 10 ether);
-        brewHouse = new BrewHouse(sequencer, address(weth));
-    }
+  function setUp() public {
+    weth = new MockWETH();
+    vm.deal(user, 10 ether);
+    vm.deal(sequencer, 10 ether);
+    brewHouse = new BrewHouse(sequencer, address(weth));
+  }
 
-    function testDepositCollateral() public {
-        vm.startPrank(user);
-        brewHouse.depositCollateral{value: 1 ether}();
-        assertEq(brewHouse.collateralBalance(user), 1 ether);
-        vm.stopPrank();
-    }
+  function testDepositCollateral() public {
+    vm.startPrank(user);
+    brewHouse.depositCollateral{value: 1 ether}();
+    assertEq(brewHouse.collateralBalance(user), 1 ether);
+    vm.stopPrank();
+  }
 
-    function testWithdrawCollateral() public {
-        vm.startPrank(user);
-        brewHouse.depositCollateral{value: 2 ether}();
-        brewHouse.withdrawCollateral(1 ether);
-        assertEq(brewHouse.collateralBalance(user), 1 ether);
-        vm.stopPrank();
-    }
+  function testWithdrawCollateral() public {
+    vm.startPrank(user);
+    brewHouse.depositCollateral{value: 2 ether}();
+    brewHouse.withdrawCollateral(1 ether);
+    assertEq(brewHouse.collateralBalance(user), 1 ether);
+    vm.stopPrank();
+  }
 
-    function testWithdrawMoreThanCollateralShouldRevert() public {
-        vm.startPrank(user);
-        brewHouse.depositCollateral{value: 1 ether}();
-        vm.expectRevert(BrewHouse.InsufficientCollateral.selector);
-        brewHouse.withdrawCollateral(2 ether);
-        vm.stopPrank();
-    }
+  function testWithdrawMoreThanCollateralShouldRevert() public {
+    vm.startPrank(user);
+    brewHouse.depositCollateral{value: 1 ether}();
+    vm.expectRevert(BrewHouse.InsufficientCollateral.selector);
+    brewHouse.withdrawCollateral(2 ether);
+    vm.stopPrank();
+  }
 
-    function testOnlySequencerCanSlash() public {
-        vm.startPrank(user);
-        brewHouse.depositCollateral{value: 1 ether}();
-        vm.stopPrank();
+  function testOnlySequencerCanSlash() public {
+    vm.startPrank(user);
+    brewHouse.depositCollateral{value: 1 ether}();
+    vm.stopPrank();
 
-        vm.startPrank(address(3)); // Not sequencer
-        vm.expectRevert(BrewHouse.OnlySequencer.selector);
-        brewHouse.slashCollateral(user, 0.5 ether);
-        vm.stopPrank();
-    }
+    vm.startPrank(address(3)); // Not sequencer
+    vm.expectRevert(BrewHouse.OnlySequencer.selector);
+    brewHouse.slashCollateral(user, 0.5 ether);
+    vm.stopPrank();
+  }
 
-    function testSequencerCanSlash() public {
-        vm.startPrank(user);
-        brewHouse.depositCollateral{value: 1 ether}();
-        vm.stopPrank();
+  function testSequencerCanSlash() public {
+    vm.startPrank(user);
+    brewHouse.depositCollateral{value: 1 ether}();
+    vm.stopPrank();
 
-        vm.startPrank(sequencer);
-        brewHouse.slashCollateral(user, 0.5 ether);
-        assertEq(brewHouse.collateralBalance(user), 0.5 ether);
-        vm.stopPrank();
-    }
+    vm.startPrank(sequencer);
+    brewHouse.slashCollateral(user, 0.5 ether);
+    assertEq(brewHouse.collateralBalance(user), 0.5 ether);
+    vm.stopPrank();
+  }
 }
-
--- test/LattePool.t.sol
+++ test/LattePool.t.sol
@@ -5,71 +5,70 @@ import "forge-std/Test.sol";
 import "../src/LattePool.sol";
 
 contract LattePoolTest is Test {
-    LattePool public pool;
+  LattePool public pool;
 
-    address user = address(0xBEEF);
-    address anotherUser = address(0xCAFE);
+  address user = address(0xBEEF);
+  address anotherUser = address(0xCAFE);
 
-    function setUp() public {
-        pool = new LattePool();
-        vm.deal(address(pool), 10 ether);  // Fund the pool with ETH
-        vm.deal(user, 5 ether);
-        vm.deal(anotherUser, 5 ether);
-    }
+  function setUp() public {
+    pool = new LattePool();
+    vm.deal(address(pool), 10 ether); // Fund the pool with ETH
+    vm.deal(user, 5 ether);
+    vm.deal(anotherUser, 5 ether);
+  }
 
-    function testBorrow() public {
-        vm.startPrank(user);
-        pool.borrow(1 ether);
-        vm.stopPrank();
+  function testBorrow() public {
+    vm.startPrank(user);
+    pool.borrow(1 ether);
+    vm.stopPrank();
 
-        assertEq(pool.debtBalance(user), 1 ether);
-        assertEq(user.balance, 6 ether); // originally 5, now +1
-    }
+    assertEq(pool.debtBalance(user), 1 ether);
+    assertEq(user.balance, 6 ether); // originally 5, now +1
+  }
 
-    function testBorrowInsufficientLiquidity() public {
-        vm.startPrank(user);
-        vm.expectRevert(LattePool.InsufficientLiquidity.selector);
-        pool.borrow(11 ether); // more than pool has
-        vm.stopPrank();
-    }
+  function testBorrowInsufficientLiquidity() public {
+    vm.startPrank(user);
+    vm.expectRevert(LattePool.InsufficientLiquidity.selector);
+    pool.borrow(11 ether); // more than pool has
+    vm.stopPrank();
+  }
 
-    function testRepay() public {
-        vm.startPrank(user);
-        pool.borrow(2 ether);
-        pool.repay{value: 1 ether}(); // repay partially
-        vm.stopPrank();
+  function testRepay() public {
+    vm.startPrank(user);
+    pool.borrow(2 ether);
+    pool.repay{value: 1 ether}(); // repay partially
+    vm.stopPrank();
 
-        assertEq(pool.debtBalance(user), 1 ether);
-    }
+    assertEq(pool.debtBalance(user), 1 ether);
+  }
 
-    function testRepayFull() public {
-        vm.startPrank(user);
-        pool.borrow(1.5 ether);
-        pool.repay{value: 1.5 ether}();
-        vm.stopPrank();
+  function testRepayFull() public {
+    vm.startPrank(user);
+    pool.borrow(1.5 ether);
+    pool.repay{value: 1.5 ether}();
+    vm.stopPrank();
 
-        assertEq(pool.debtBalance(user), 0);
-    }
+    assertEq(pool.debtBalance(user), 0);
+  }
 
-    function testRepayTooMuchRevert() public {
-        vm.startPrank(user);
-        pool.borrow(1 ether);
-        vm.expectRevert(LattePool.RepayTooMuch.selector);
-        pool.repay{value: 2 ether}(); // can't repay more than debt
-        vm.stopPrank();
-    }
+  function testRepayTooMuchRevert() public {
+    vm.startPrank(user);
+    pool.borrow(1 ether);
+    vm.expectRevert(LattePool.RepayTooMuch.selector);
+    pool.repay{value: 2 ether}(); // can't repay more than debt
+    vm.stopPrank();
+  }
 
-    function testMultipleUsersIndependentDebt() public {
-        vm.startPrank(user);
-        pool.borrow(1 ether);
-        vm.stopPrank();
+  function testMultipleUsersIndependentDebt() public {
+    vm.startPrank(user);
+    pool.borrow(1 ether);
+    vm.stopPrank();
 
-        vm.startPrank(anotherUser);
-        pool.borrow(2 ether);
-        vm.stopPrank();
+    vm.startPrank(anotherUser);
+    pool.borrow(2 ether);
+    vm.stopPrank();
 
-        assertEq(pool.debtBalance(user), 1 ether);
-        assertEq(pool.debtBalance(anotherUser), 2 ether);
-    }
+    assertEq(pool.debtBalance(user), 1 ether);
+    assertEq(pool.debtBalance(anotherUser), 2 ether);
+  }
 }
-
